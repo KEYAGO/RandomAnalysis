d8a9662b04f2721ea250aad5ec72c904afbf5e38
KEYAGO <yasingoka@gmail.com>
2026-07-17 15:01:20 +0300

    Fix layout paths and clean project files

--- start_local.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo RandomAnalysis Sunucusu Baslatiliyor...
-:: Sanal ortami aktif et
-call venv\Scripts\activate
-:: FastAPI sunucusunu baslat (app.py dosyasini referans alir)
-uvicorn app:app --reload
-pause
